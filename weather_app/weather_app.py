dfa5f728370ad58e5da02df7a953610128c2d5c7
Geri Dashja <geridashjaa@gmail.com>
2020-11-19 20:26:15 +0300

    first

---
+++ weather_app/weather_app.py
@@ -0,0 +1,71 @@
+from tkinter import *
+from tkinter import messagebox
+from configparser import ConfigParser
+import requests
+
+
+
+url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid={}'
+
+config_file = "config.ini"
+config = ConfigParser()
+config.read(config_file)
+api_key = config['api_key']['key']
+
+
+def search():
+    global image
+
+    city = city_text.get()
+    weather = get_infos(city)
+    if weather:
+        Location["text"] = "{}, {}". format(weather[0], weather[1])
+        Image["bitmap"] = "icons/{}.png".format(weather[4])
+        Temperature["text"] = "{:.2f}°C, {:.2f}°F".format(weather[2], weather[3])
+        Weather["text"] = "{}".format(weather[5])
+    else:
+        messagebox.showerror("Error", "Cannot find the {}".format(city))
+
+
+def get_infos(city):
+    result = requests.get(url.format(city, api_key))
+    if result:
+        json = result.json()
+        city = json["name"]
+        temp_kelvin = json["main"]["temp"]
+        temp_celsius = temp_kelvin - 273.15
+        temp_far = (temp_kelvin - 273.15) * 9/5 + 32
+        country = json["sys"]["country"]
+        icon = json["weather"][0]["icon"]
+        weather = json["weather"][0]["main"]
+        final = (city, country, temp_celsius, temp_far, icon, weather)
+        return final
+    else:
+        return None
+
+
+root = Tk()
+blank_space = " "
+root.title(90*blank_space+"Weather Checker")
+root.geometry("700x350")
+
+city_text = StringVar()
+city_entry = Entry(root, textvariable=city_text)
+city_entry.pack()
+
+Search = Button(root, text="Search Weather", command=search)
+Search.pack()
+
+Location = Label(root, text="", font=("bold", 18))
+Location.pack(pady=8)
+
+Image = Label(root, bitmap='')
+Image.pack()
+
+Temperature = Label(root, text="")
+Temperature.pack()
+
+Weather = Label(root, text='')
+Weather.pack()
+
+root.mainloop()
